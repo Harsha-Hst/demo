f8e3cc97012575281905a6df88baa59644ad1448
Harsha <harshahst5@gmail.com>
2026-06-30 20:39:17 +0530

    Sum of 2 numbers

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number :");
+    int n1 = sc.nextInt();
+    System.out.println("Enter a number :");
+    int n2 = sc.nextInt();
+    int sum = n1 + n2;
+    System.out.println("Sum of 2 numbers : " + sum);
+    sc.close();
+    }
+    
+}
